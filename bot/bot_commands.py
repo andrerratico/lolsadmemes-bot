0a6feeab7242a74371fc7b25d5179e1ffa5af1b7
moonR2 <moonyneverend@gmail.com>
2021-07-01 00:31:40 -0500

    Nice msg parsing with embed

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -1,4 +1,5 @@
 import os
+import discord
 from discord_client import init_client
 from discord.ext import commands 
 from discord.ext.commands import Context
@@ -12,7 +13,9 @@ async def get_stats(ctx, summoner):
     invoker = riot.get_sumId(summoner)
     id = invoker['id']
     stats = riot.get_stats(id)
-    await ctx.send(stats)
+    msg = discord.Embed(title="Solo/Duo Stats", description="", color=0x5CD1E5)
+    msg.add_field(name=f"Ranked Solo: {stats[0]['tier']} {stats[0]['rank']}", value=f"{stats[0]['leaguePoints']}lp", inline=False)
+    await ctx.send(embed = msg)
 
 bot.run(TOKEN)
 
